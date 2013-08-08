47628a7a6c0c5d9ef6cf950e52cdc89fcd803360
Les Orchard <me@lmorchard.com>
2013-08-08 10:54:56 -0400

    Tweaks to setup.py and packaging

--- setup.py
+++ setup.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python
-from setuptools import setup
-try:
-    import multiprocessing
-except ImportError:
-    pass
-
+from setuptools import setup, find_packages
 
 setup(
     name='django-valet-keys',
-    version='0.0.1',
+    version='0.0.2',
     description='Django app for managing valet keys for robots',
     long_description=open('README.rst').read(),
     author='Les Orchard',
     author_email='me@lmorchard.com',
     url='http://github.com/lmorchard/django-valet-keys',
-    license='BSD',
-    packages=['valet_keys'],
-    package_data={},
+    license='MPL',
+    packages=find_packages(exclude=['valet_keys_example*']),
+    include_package_data=True,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
